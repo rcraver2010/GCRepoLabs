dd1df19858427c80172545634698f04cf3e38b81
Rica Craver <rica.craver@yahoo.com>
2022-10-19 17:25:55 -0400

    Finished

---
+++ counting/counting.js
@@ -0,0 +1,16 @@
+for(let i = 0; i < 10; i++) {
+    console.log(`${i}`);
+}
+
+for(let i = 0; i < 10; i--) {
+    console.log(`${i}`);
+}
+
+do {
+    let i = 0; i++;
+}
+while (i < 10)
+console.log (`${i}`);
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+let text = "";
