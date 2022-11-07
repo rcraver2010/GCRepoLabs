0942a1d98de7480c3f035856ae4ec7fff8913d9e
Rica Craver <rica.craver@yahoo.com>
2022-11-07 11:42:04 -0500

    done

---
+++ menu/menu.js
@@ -0,0 +1,15 @@
+
+function showLetters() {
+    document.getElementById("letters").style.display = "block";
+    document.getElementById("numbers").style.display = "none";
+  }
+
+function showNumbers() {
+    document.getElementById("letters").style.display = "none";
+    document.getElementById("numbers").style.display = "block";
+}
+
+function menu() {
+    document.getElementById("letters").style.display = "block";
+    document.getElementById("numbers").style.display = "none"; 
+}
